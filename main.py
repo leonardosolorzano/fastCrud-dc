d9e1abd7684d4da1fac36a19202da1af0d9897b1
leo <ivanpacheco.solorzano@gmail.com>
2026-03-23 17:26:42 -0500

    inicio del projecto, file que correra el projecto

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from app.config import settings
+from app.routers import router as status_router
+from app.db import engine, SessionLocal
+from app import models, db
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+    description=settings.APP_DESCRIPTION,
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.include_router(status_router)
+
+@app.get("/")
+def root():
+    return {"message": "API CRUD con FastAPI"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
