042da32d0b0472eb86e241a822bcfc902d6084fd
leo <ivanpacheco.solorzano@gmail.com>
2026-04-21 14:32:19 -0500

    first commit

--- app/routers.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from .schemas.post_schemas import PostRead
-from .models.post_model import Post
-
-router = APIRouter(prefix="/post", tags=["post"])
-
-@router.get("/", response_model=PostRead)
-def get_all_posts():
-    return PostRead(posts=[Post(post="ok")])
